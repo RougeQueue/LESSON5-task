e6db916eb9773d845161eab1084cf234b777c0ae
RougeQueue <117088790+RougeQueue@users.noreply.github.com>
2022-11-18 23:34:01 +0900

    Update app.js

--- app.js
+++ app.js
@@ -5,25 +5,14 @@ while(num < 50){
   num = num + 1;
     count++;
   console.log(count);
-  if(count % 10 == 0 , count < 50){
-    console.log("今" + count + "回ループしました");
-  }else if(num % 4 == 0) {
-    console.log("今" + count + "回ループしました");
-  }else if(count == 50){
-    alert(count + "回カウントが終わりました");
-  }
-}
-
-/*while(num < 50){
-  num = num + 1;
-    count++;
-  console.log(count);
   if(count == 50){
     alert(count + "回カウントが終わりました");
+  }else if(num % 4 == 0 && count % 10 == 0){
+    console.log("4で割れる数です。" );
+    console.log("今" + count + "回ループしました。");
   }else if(num % 4 == 0) {
-    console.log("今" + count + "回ループしました");
-  }else if
-  (count % 10 == 0){
-    console.log("今" + count + "回ループしました");
+    console.log("4で割れる数です。" + count + "回ループしました。");
+  }else if(count % 10 == 0){
+    console.log("今" + count + "回ループしました。");
   }
-}*/
+}
